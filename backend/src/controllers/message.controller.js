91d2a66d1d2d04e732fe2c8a946817b964b45260
Laiba <laibatariq110@gmail.com>
2026-04-27 22:11:56 +0500

    fix real-time messaging by emitting to both sender and receiver

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -58,8 +58,14 @@ export const sendMessage = async (req, res) => {
     await newMessage.save();
     // realtime functionality goes here => socket.io
     const receiverSocketId = getReceiverSocketId(receiverId);
+    const senderSocketId = getReceiverSocketId(senderId);
+
     if (receiverSocketId) {
       io.to(receiverSocketId).emit("newMessage", newMessage);
+    }
+
+    if (senderSocketId) {
+      io.to(senderSocketId).emit("newMessage", newMessage);
     }
 
     res.status(201).json(newMessage);
