a0c0f2c268eee2a91c7f464bdc875a04dc808998
Laiba <laibatariq110@gmail.com>
2026-04-27 21:28:41 +0500

    fix socket listener duplication in chat

--- frontend/src/store/useChatStore.js
+++ frontend/src/store/useChatStore.js
@@ -56,6 +56,8 @@ export const useChatStore = create((set, get) => ({
 
     const socket = useAuthStore.getState().socket;
 
+    socket.off("newMessage");
+
     socket.on("newMessage", (newMessage) => {
       const isMessageSentFromSelectedUser = newMessage.senderId === selectedUser._id;
       if (!isMessageSentFromSelectedUser) return;
