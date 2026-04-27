592bf903c1df8910ebf2bcdf883dd1f9a5a4044e
Laiba <laibatariq110@gmail.com>
2026-04-27 12:07:10 +0500

    fix deploy + add dynamic CORS

--- backend/src/index.js
+++ backend/src/index.js
@@ -13,16 +13,16 @@ import path from "path";
 
 dotenv.config();
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5001;
 const __dirname = path.resolve();
 
 app.use(express.json({ limit: "5mb" }));
 app.use(cookieparser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-  }),
+  origin: process.env.CLIENT_URL || "http://localhost:5173",
+  credentials: true,
+})
 );
 
 app.use("/api/auth", authRoutes);
@@ -31,13 +31,12 @@ app.use("/api/messages", messageRoutes);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.get("/*", (req,res) => {
+  app.use((req, res) => {
     res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
-  })
+  });
 }
 
 server.listen(PORT, () => {
   console.log("server is running on port PORT: " + PORT);
   connectDB();
 });
-console.log("MONGO:", process.env.MONGODB_URI);
