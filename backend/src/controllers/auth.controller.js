5e09455be16b20a6ad9c338b27c27d8021f5b7df
Laiba <laibatariq110@gmail.com>
2026-04-28 16:57:45 +0500

    fix: _id bug causing chat issues

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -59,7 +59,7 @@ export const login = async (req, res) => {
 
     generateToken(user._id, res);
     res.status(200).json({
-      id: user._id,
+      _id: user._id,
       fullName: user.fullName,
       email: user.email,
       profilePic: user.profilePic,
